f87fc4acd8d8c0101e2ef82cc8e159cbf9879787
NekosanQ <NekosanQ@outlook.com>
2023-09-10 03:00:05 +0900

    ステータスメッセージ少し変えただけ！w

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -12,7 +12,7 @@ module.exports = {
 		const date = new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' });
 		setInterval(() => {
 			client.user?.setActivity({
-				name: `猫の隠れ家 専属BOT | / |${client.ws.ping}ms | 最終更新: ${date}`
+				name: `/ | 猫の隠れ家 専属BOT | ${client.ws.ping}ms | 最終更新: ${date}`
 			});
 		  }, 10000);
 		logger.info(`[INFO] 起動完了: ${client.user?.tag}`);
