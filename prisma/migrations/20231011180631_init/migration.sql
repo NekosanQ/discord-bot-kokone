561c020a9123ab7468994f285aa86338a5ab94b2
NekosanQ <NekosanQ@outlook.com>
2023-12-17 18:35:12 +0900

    スキーマ変更

--- prisma/migrations/20231011180631_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `BlackLists` (
-    `user_id` INTEGER NOT NULL AUTO_INCREMENT,
-    `userId` VARCHAR(191) NOT NULL,
-    `blockUserId` VARCHAR(191) NOT NULL,
-
-    PRIMARY KEY (`user_id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
