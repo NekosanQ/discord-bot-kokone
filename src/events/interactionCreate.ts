cb0fa0a3c92f4c94a5eea96be4887456f8d4ed1f
NekosanQ <NekosanQ@outlook.com>
2023-12-27 21:54:44 +0900

    バグ修正

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -29,7 +29,7 @@ module.exports = {
 			};
 		} else if (!command) {
 			try {
-				await require("../events/interactionDiscrimination").execute(interaction);
+				await require("../guildProcess/voiceCreateInteraction").execute(interaction);
 			} catch (error) {
 				console.log(error);
 			}
--- src/events/interactionDiscrimination.ts
+++
@@ -1,7 +0,0 @@
-import { Interaction } from 'discord.js';
-
-module.exports = {
-	async execute(interaction: Interaction): Promise<void> {
-        await require("../guildProcess/voiceCreateInteraction").execute(interaction);
-    }
-};
