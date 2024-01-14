d6e87a4e599e199f2461560c4b0ea19a1555e99f
NekosanQ <NekosanQ@outlook.com>
2024-01-15 07:44:12 +0900

    update

--- README.md
+++
@@ -1,6 +0,0 @@
-# DiscordBOT-Kokone
-猫の隠れ家-CatHideaway 専属BOTのソースコードです。
-まだ書き直し部分が多いです...
-
-猫の隠れ家-CatHideawayのDiscordサーバー
-https://discord.gg/cathideaway
--- src/module/file/appedFile.ts
+++
@@ -1,8 +0,0 @@
-import fs from "node:fs"
-
-//ファイルの書き込み関数
-export function appendFile(path: string, data: string) {
-    fs.appendFile(path, data, (err) => {
-        if (err) throw err;
-    });
-};
