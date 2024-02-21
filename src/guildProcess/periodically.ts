25c6dd69fc109d3c3d97e7071e04e9500da07617
NekosanQ <NekosanQ@outlook.com>
2024-02-21 19:49:23 +0900

    変数名の変更

--- src/guildProcess/periodically.ts
+++ src/guildProcess/periodically.ts
@@ -12,14 +12,14 @@ module.exports = {
     async execute(client: Client): Promise<void> {
         const date: string = new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' });
         try {
-            const getlogChannelId: Channel | undefined = client.channels.cache.get(config.logChanneld); // 運営チャンネルを取得
-            if (getlogChannelId) {
+            const getLogChannelId: Channel | undefined = client.channels.cache.get(config.logChanneld); // 運営チャンネルを取得
+            if (getLogChannelId) {
                 const logsPath: string = path.join(__dirname, '../../logs');
                 const logFiles: string[] = fs.readdirSync(logsPath).filter(file => file.endsWith('.log'));
                 cron.schedule("0 0 0 * * *", () => { // 0時になったら実行
                     for (const file of logFiles) { // logsフォルダのlogファイルを全て取得
                         const filePath: string = path.join(logsPath, file);
-                        (getlogChannelId as TextChannel).send({
+                        (getLogChannelId as TextChannel).send({
                             files: [filePath]
                         })
                         setTimeout(() => { // ファイルを送信した1秒後に上書きする
